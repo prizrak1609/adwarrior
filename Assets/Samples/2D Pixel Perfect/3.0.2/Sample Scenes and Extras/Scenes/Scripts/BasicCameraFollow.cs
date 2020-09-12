eb0c4492a0f8e74e2fc494c068c4777916ed5961
Dima Gubatenko <gubatenkod@gmail.com>
2020-09-12 13:36:22 +0300

    Initial commit

---
+++ Assets/Samples/2D Pixel Perfect/3.0.2/Sample Scenes and Extras/Scenes/Scripts/BasicCameraFollow.cs
@@ -0,0 +1,22 @@
+﻿// ReSharper disable Unity.InefficientPropertyAccess
+// ReSharper disable CheckNamespace
+
+using UnityEngine;
+
+public class BasicCameraFollow : MonoBehaviour
+{
+	public GameObject followTarget;
+	private Vector3 _targetPos;
+	public float moveSpeed;
+	
+	void Update () 
+	{
+		var targetPosition = followTarget.transform.position;
+		var transformPosition = transform.position;
+		
+		_targetPos = new Vector3(targetPosition.x, targetPosition.y, transform.position.z);
+		
+		var velocity = _targetPos - transform.position;
+		transform.position = Vector3.SmoothDamp (transform.position, _targetPos, ref velocity, 1.0f, moveSpeed * Time.deltaTime);	
+	}
+}
